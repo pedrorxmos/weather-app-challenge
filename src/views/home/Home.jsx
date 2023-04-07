45c41244d08e1e2bc6b0fc1b9a94baa72b90bb07
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-04-07 14:40:09 +0200

    💄 feat(home): add actions

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,9 +1,18 @@
 import './home.scss';
+import { LocationSVG } from '../../assets/icons';
 
 export function Home() {
   return(
     <header className="home">
-      <p>Home.jsx</p>
+      <nav className="home__actions">
+        <button className="btn action__search">
+          Search for places
+        </button>
+
+        <button className="btn btn-round btn-icon">
+          <LocationSVG/>
+        </button>
+      </nav>
     </header>
   )
 }
