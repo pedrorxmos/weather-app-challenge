0967c8f7d5b17917ccc0d2f975cf0def8987cde4
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-04-09 21:18:12 +0200

    💄 feat(aside): day cards

--- src/components/day/DayComponent.jsx
+++ src/components/day/DayComponent.jsx
@@ -4,8 +4,10 @@ export function DayComponent({date, type, max, min}) {
 
   return(
     <div className="day-card">
-      <p className="day__date">{date}</p>
-      <img src={`/src/assets/img/${type}.webp`} alt={`${date} weather`} />
+      <div className="day__group">
+        <p>{date}</p>
+        <img src={`/src/assets/img/${type}.webp`} alt={`${date} weather`} />
+      </div>
       <div className="day__maxmin">
         <p>{max}<span>ºC</span></p>
         <p>{min}<span>ºC</span></p>
